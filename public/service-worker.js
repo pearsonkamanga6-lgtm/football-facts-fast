14582ce9281bd86f73da01d0872d067284f06db1
pearsonkamanga6-lgtm <pearsonkamanga6@gmail.com>
2026-09-16 21:19:03 +0200

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="factfirst-v3-8";
+const APP=["/","/manifest.webmanifest","/icon-192.png","/icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))]));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET"||new URL(e.request.url).pathname.startsWith("/api/"))return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
